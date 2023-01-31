6b3d0c2c064012ff2e71f75f60fc55391611c83b
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-01-31 12:05:07 -0600

    Assignment01

--- Gurram_Assignment01/Gurram_Assignment01/ViewController.swift
+++ Gurram_Assignment01/Gurram_Assignment01/ViewController.swift
@@ -8,34 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var fullNameLabel: UITextField!
+    
+  
+    @IBOutlet weak var firstNameOutlet: UITextField!
     
     
-    @IBOutlet weak var InitialsLabel: UITextField!
+    @IBOutlet weak var lastNameOutlet: UITextField!
+    
+    @IBOutlet weak var yearOutlet: UITextField!
     
     
     
-    @IBOutlet weak var ageLabel: UITextField!
+    @IBOutlet weak var details: UILabel!
     
     
-    @IBOutlet weak var firstNameOutlet: UILabel!
+    @IBOutlet weak var fullNameLabel: UILabel!
     
     
-    @IBOutlet weak var lastNameOutlet: UILabel!
+    @IBOutlet weak var InitialsLabel: UILabel!
     
-    @IBOutlet weak var yearOutlet: UILabel!
     
+    @IBOutlet weak var ageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func SubmitBTN(_ sender: UIButton) {
-        var firstNameOutlet = fullNameLabel.text!
-        var lastNumberOutlet = InitialsLabel.text!
+        var lastName = lastNameOutlet.text!
+        var firstName = firstNameOutlet.text!
+        details.text = "Details"
+        fullNameLabel.text = "Full Name : \(lastName) \(firstName)"
         
-        print("\(lastNumberOutlet) \(firstNameOutlet)")
+        var initialLast = lastName.first!
+        var initialFirst = firstName.first!
+        InitialsLabel.text = "Initials : \(initialLast) \(initialFirst)"
+        let yearBirth = yearOutlet.rawValue
+        let date = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year], from: date)
+        let year = components.year!
+        
+        let age = yearBirth - year
+        ageLabel.text = "Age : \(age)"
       
     }
     

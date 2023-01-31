57962d5df2b37442fa1f5827209f19015c6b19c2
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-01-31 12:06:57 -0600

    Assignment1 PlayGround

--- Gurram_Swift_Assignment01.playground/Contents.swift
+++ Gurram_Swift_Assignment01.playground/Contents.swift
@@ -14,15 +14,30 @@ print(Sum)
       print("----------------------")
 // End of question 2
 // 3. Calculate the area(rounded to 2 decimal points) of a rhombus with diagonal values as 25 and 26.8 respectively. Print the result obtained as "The area of the rhombus with given diagonal values is *****", replace the **** with the area. [Area = Diagonal_1*Diagonal_2]/2
+
+var d1 = 25
+var d2 = 26.8
+var area1 = (Double(d1) * d2)/2
+print("The area of the rhombus with given diagonal values is \(area1)")
     print("----------------------")
 // End of question 3
 // 4. Using 3 print statements write something about you in 3 sentences, one for each print but print first 2 statements in single line and next statement in the 2nd line.
-   print("----------------------")
+
+print("My name is Raviteja Gurram ", terminator:"")
+print("I am from INDIA ")
+print("I am here to do my masters in ACS ")
+print("----------------------")
 // End of question 4
 // 5. Using one print statement display the following 3M
 // "Swift is a powerful and intuitive programming language for iOS, iPadOS, macOS, tvOS, and watchOS.
 //   Writing Swift code is interactive and fun, the syntax is concise yet expressive, and Swift includes modern features   developers love.
 //   Swift code is safe by design and produces software that runs lightning-fast."
+
+print("""
+Swift is a powerful and intuitive programming language for iOS, iPadOS, macOS, tvOS, and watchOS.
+Writing Swift code is interactive and fun, the syntax is concise yet expressive, and Swift includes modern features   developers love.
+Swift code is safe by design and produces software that runs lightning-fast
+""")
 print("----------------------")
 // End of question 5
 // 6. Print a Multiplication table of any number of your choice starting from 1 to 10
@@ -34,6 +49,12 @@ for index in 1...10 {
 print("----------------------")
 // End of question 6
 // 7. Calculate the area and perimeter of a square with side = 8 , and if the area and perimeter is more than 20 display a message stating that "The area of the given square is ***** and perimeter is *****" else display "Area and perimeter is less than 20"[NOTE : replace stars with area and perimeter respectively.]
+var side:Int = 8;
+var area = side*side
+var perimeter = 4*side
+if( area > 20  && perimeter > 20){
+    print("The area of the given square is \(area) and perimeter is \(perimeter)")
+}
 
 print("----------------------")
 // End of question 7

746c1d842d04a299c4154a9360288cbb418c2fdc
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-02-15 19:52:59 -0600

    Add files via upload

---
+++ Gurram_CalculatorApp/Gurram_CalculatorApp/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Gurram_CalculatorApp
+//
+//  Created by Gurram,Sai Venkata Raviteja on 2/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultOutlet: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonAC(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonC(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonPlusOrMinus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonDivision(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonSeven(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonEight(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonNine(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonMultiply(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonFour(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonFive(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonSix(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonMinus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonOne(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonTwo(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonThree(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonPlus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonZero(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func buttonDot(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonModulus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonEquals(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+}
+

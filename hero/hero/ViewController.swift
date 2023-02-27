a76eb11b445e13c39d885aa8859abaab02d170e2
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-02-27 16:24:50 -0600

    Add files via upload

---
+++ hero/hero/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  hero
+//
+//  Created by Gurram,Sai Venkata Raviteja on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var tex: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func click(_ sender: UIButton) {
+        
+    
+        
+    }
+    
+}
+

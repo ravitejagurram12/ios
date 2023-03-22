6b93f935f735bc86f1de27e92ffaa28fd75ec4a4
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-03-21 20:08:31 -0500

    Add files via upload

---
+++ Gurram_SearchApp/Gurram_SearchApp/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Gurram_SearchApp
+//
+//  Created by Gurram,Sai Venkata Raviteja on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func ShowPrevImagesBtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func ResetBtn(_ sender: UIButton) {
+    }
+    
+    
+
+    @IBAction func ShowNextImagesBtn(_ sender: UIButton) {
+    }
+    
+    
+}
+

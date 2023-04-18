5da1d3bd69ab3ff8d5236c86975edc5185c07f57
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-04-18 13:45:24 -0500

    Add files via upload

---
+++ Gurram_UniversityApp/Gurram_UniversityApp/UniversitiesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Gurram_UniversityApp
+//
+//  Created by Gurram,Sai Venkata Raviteja on 4/18/23.
+//
+
+import UIKit
+
+class UniversitiesViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var universitiesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

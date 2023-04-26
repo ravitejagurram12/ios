cc93736246d0f4390fd51d18c1836aae87e7d5b4
Raviteja Gurram <111926089+ravitejagurram12@users.noreply.github.com>
2023-04-26 14:36:48 -0500

    Add files via upload

---
+++ Gurram_Movies/Gurram_Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gurram_Movies
+//
+//  Created by Gurram,Sai Venkata Raviteja on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
